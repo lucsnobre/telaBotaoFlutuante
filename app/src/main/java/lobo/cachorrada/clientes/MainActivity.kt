23e140a47384b9d57d4436ef36072b9a066efd01
LUCAS RODRIGUES NOBRE SILVA <24122499@dsn127m1242314.cfp127aluno.local>
2025-08-05 11:40:43 -0300

    sapo

--- app/src/main/java/lobo/cachorrada/clientes/MainActivity.kt
+++ app/src/main/java/lobo/cachorrada/clientes/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import lobo.cachorrada.clientes.screens.HomeScreen
 import lobo.cachorrada.clientes.ui.theme.ClientesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ClientesAppTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
